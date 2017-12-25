c0e2790d05ade2437387fee95e8a1ba279a249aa
xyxiao <463618622@qq.com>
2017-12-25 17:51:20 +0800

    add some

--- studyTs/4.类/index.ts
+++ studyTs/4.类/index.ts
@@ -12,3 +12,22 @@
   let greeter = new Greeter('123!')
   console.log(greeter.greet())
 }
+
+// 继承
+{
+  class Animal {
+    move (distanceInMeters: number = 0) {
+      console.log(`Animal moved ${distanceInMeters}m`)
+    }
+  }
+
+  class Dog extends Animal {
+    bark () {
+      console.log('Woof! Woof!')
+    }
+  }
+
+  const dog = new Dog()
+  dog.bark()
+  dog.move(10)
+}
